d093ff3ebc1be7088130f017f8bc35c1dd022220
CoderNotCold <CoderNotCold@gmail.com>
2022-06-20 00:04:57 +0800

    add byCast

--- main.c
+++ main.c
@@ -7,15 +7,33 @@ union UnionDetectEndian {
     uint16_t u16;
 };
 
-int main() {
+void byUnion() {
     union UnionDetectEndian theUnion;
     theUnion.u16 = 0x1234;
     if (theUnion.u8 == 0x12) {
-        printf("big endian\n");
+        printf("byUnion:big endian\n");
     } else if (theUnion.u8 == 0x34) {
-        printf("little endian\n");
+        printf("byUnion:little endian\n");
     } else {
-        printf("detect endian failed!\n");
+        printf("byUnion:detect endian failed!\n");
     }
+}
+
+void byCast() {
+    uint16_t a = 0x1234;
+    uint8_t firstByte = *((uint8_t *) &a);
+    if (firstByte == 0x12) {
+        printf("byCast:big endian\n");
+    } else if (firstByte == 0x34) {
+        printf("byCast:little endian\n");
+    } else {
+        printf("byCast:detect endian failed!\n");
+    }
+}
+
+int main() {
+    byUnion();
+    byCast();
     return 0;
 }
+
